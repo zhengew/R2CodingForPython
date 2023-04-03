ef5298a9fb58cac57173ea849b95a24da2c0d04e
erwei.zheng <zhengew@foxmail.com>
2023-04-03 09:11:30 +0800

    sys.argv

--- day22_oop第一天/exam/walk_练习题.py
+++ day22_oop第一天/exam/walk_练习题.py
@@ -6,3 +6,29 @@
 """
 使用os.walk 计算文件夹下的文件大小
 """
+import os
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+def get_file_size(path: str):
+    """
+    文件夹大小
+    :param path:
+    :return:
+    """
+    ret = os.walk(path)
+    file_size_sum = 0
+    for i in ret:
+        file_path, files = i[0], i[2]
+        for file in files:
+            abs_path = os.path.join(file_path, file)
+            file_size = os.path.getsize(abs_path)
+            file_size_sum += file_size
+            logging.debug(f"当前文件路径:{abs_path}, 当前文件大小:{file_size}")
+            logging.debug(f"文件总大小: {file_size_sum}")
+    return file_size_sum
+
+if __name__ == '__main__':
+    path = "/home/zew/PycharmProjects/R2CodingForPython/day22_oop第一天/exam"
+    file_size_sum = get_file_size(path)
+    print(file_size_sum)
