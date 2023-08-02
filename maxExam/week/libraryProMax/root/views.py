956819c45cb5a1ffc1eec4ddefaa64f6b878e8dd
erwei.zheng <zhengew@foxmail.com>
2023-08-02 17:31:04 +0800

    django_多表大作业V2.0

--- maxExam/week/libraryProMax/root/views.py
+++ maxExam/week/libraryProMax/root/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.views import View
 from django.urls import reverse # 反向解析url
+from root import models
 # Create your views here.
 
 class RootApp(View):
@@ -17,4 +18,31 @@ class RootApp(View):
     def post(self, request, *args, **kwargs):
 
         if request.path == '/':
-            return render(request, 'home.html')
+            loginName = request.POST.get('username')
+            loginPwd = request.POST.get('password')
+            if models.UserInfo.objects.filter(username=loginName).exists():
+                pwd = models.UserInfo.objects.filter(username=loginName).values('password')[0]['password']
+                if loginPwd == pwd:
+                    all_books = self.query_all_books()
+                    return render(request, 'home.html', {'all_books': all_books})
+                else:
+                    return redirect('login')
+            else:
+                return redirect(reverse('login'))
+
+
+
+    def query_all_books(self):
+        '''
+        select
+	        rb.title, rb.price, rb.publishDate, rp.name as publishName, ra.name as authorName
+         from root_book rb
+         inner join root_publish rp on rb.publishs_id = rp.id
+         inner join root_book_authors rba  on rb.id = rba.book_id
+         inner join root_author ra on rba.author_id = ra.id
+         order by rb.id asc
+        '''
+
+        ret = models.Book.objects.all().values('title', 'price', 'publishDate', 'publishs__name', 'authors__name')
+        print(ret)
+        return ret
