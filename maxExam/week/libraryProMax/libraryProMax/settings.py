2071bc3e047feb27a9bf266fb0acd3e74580e474
erwei.zheng <zhengew@foxmail.com>
2023-08-06 20:12:24 +0800

    django_多表大作业V2.3_已修复多个作者会显示重复数据问题，添加、删除、编辑功能已完成

--- maxExam/week/libraryProMax/libraryProMax/settings.py
+++ maxExam/week/libraryProMax/libraryProMax/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'libraryProMax',
-        'HOST': '127.0.0.1',
+        'HOST': '172.16.238.5',
         'PORT': 3306,
         'USER': 'root',
         'PASSWORD': '123456',
--- maxExam/week/libraryProMax/root/views.py
+++ maxExam/week/libraryProMax/root/views.py
@@ -3,6 +3,8 @@ from django.views import View
 from django.urls import reverse # 反向解析url
 from root import models
 # Create your views here.
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 class RootApp(View):
 
@@ -17,25 +19,37 @@ class RootApp(View):
 
         elif request.path == '/addBook/':
             all_publishs = self.query_all_publishs()
-            print(all_publishs)
             all_authors = self.query_all_authors()
             return render(request, 'add_book.html', {'all_publishs': all_publishs, 'all_authors': all_authors})
 
         elif request.path == '/queryBook/':
             all_books = self.query_all_books()
-            print(all_books)
             return render(request, 'home.html', {'all_books': all_books})
 
         elif request.path.startswith('/delBook/'):
             book_id = kwargs['book_id']
             root_book_authors_id = [id['authors__id'] for id in models.Book.objects.filter(id=book_id).values('authors__id')]
-            print(root_book_authors_id)
             # 删除中间表和书籍表
             models.Book.objects.get(id=book_id).authors.remove(*root_book_authors_id)
             models.Book.objects.filter(id=book_id).delete()
             # 重定向
             return redirect(reverse('queryBook'))
 
+        elif request.path.startswith('/editBook/'):
+            id = kwargs['book_id']
+            book_obj = models.Book.objects.get(id=id)
+            title = book_obj.title
+            price = book_obj.price
+            publishDate = book_obj.publishDate
+            publishID = book_obj.publishs.id
+            authorID = [book['id'] for book in book_obj.authors.values('id')]
+            all_publishs = models.Publish.objects.all()
+            all_authors = models.Author.objects.all()
+
+            return render(request, 'edit_book.html', {'id': id, 'title': title, 'price': price,
+                                                      'publishDate': publishDate, 'publishID': publishID,
+                                                      'authorID': authorID, 'all_publishs': all_publishs,
+                                                      'all_authors': all_authors})
 
     def post(self, request, *args, **kwargs):
 
@@ -58,7 +72,6 @@ class RootApp(View):
                 bookPublishDate = request.POST.get('inputBookPublishDate')
                 bookPublish = request.POST.get('selectBookPublisher')
                 bookAuthors = request.POST.getlist('selectBookAuthor')
-                print(bookAuthors)
                 # 书籍
                 models.Book.objects.create(**{'title': bookName, 'price': bookPrice, 'publishDate': bookPublishDate,
                                               'publishs_id': bookPublish})
@@ -69,23 +82,41 @@ class RootApp(View):
             else:
                 return redirect(reverse('queryBook'))
 
+        elif request.path.startswith('/editBook/'):
+            id = kwargs['book_id']
+            title = request.POST.get('bookName')
+            price = request.POST.get('bookPrice')
+            publishDate = request.POST.get('bookPublishDate')
+            publishID = request.POST.get('selectBookPublisher')[0]
+            author = request.POST.getlist('selectBookAuthor')
+            # 修改书籍信息
+            models.Book.objects.filter(id=id).update(
+                title=title,
+                price=price,
+                publishDate=publishDate,
+                publishs_id=publishID,
+            )
+            # 中间表 先删除再添加
+            book_obj = models.Book.objects.get(id=id)
+            book_obj.authors.clear()
+            book_obj.authors.add(*author)
 
-
+            # 重定向到查询页面
+            return redirect(reverse('queryBook'))
 
     # 查询所有书籍信息
     def query_all_books(self):
-        '''
-        select
-	        rb.title, rb.price, rb.publishDate, rp.name as publishName, ra.name as authorName
-         from root_book rb
-         inner join root_publish rp on rb.publishs_id = rp.id
-         inner join root_book_authors rba  on rb.id = rba.book_id
-         inner join root_author ra on rba.author_id = ra.id
-         order by rb.id
-        '''
-
-        all_books = models.Book.objects.all().values('id', 'title', 'price', 'publishDate', 'publishs__name', 'authors__name') # TODO: 联名作者需要处理下，作者名,分隔
-
+        all_books = models.Book.objects.all().values('id', 'title', 'price', 'publishDate', 'publishs__name', 'authors__name').order_by('id') # TODO: 联名作者需要处理下，作者名,分隔
+        booksID = set()
+        ret = {}
+        for book in all_books:
+            id = book['id']
+            if id not in booksID:
+                ret[id] = book
+                booksID.add(id)
+            elif id in booksID:
+                ret[id]['authors__name'] = ', '.join([ret[id]['authors__name'], book['authors__name']])
+        all_books = [book for book in ret.values()]
         return all_books
 
     # 查询所有出版社
