516e46867ab4a9bc6726365d336469652a2f7755
erwei.zheng <zhengew@foxmail.com>
2023-08-22 23:24:23 +0800

    day65_django_cookie 练习

---
+++ day65_django_cookie和session/exam/cookietest/root/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from root import models
+# Register your models here.
+
+# 用户表
+admin.site.register(models.UserInfo)
---
+++ day65_django_cookie和session/exam/cookietest/root/views.py
@@ -0,0 +1,59 @@
+import os
+
+from django.shortcuts import render, HttpResponse, redirect
+from django.urls import reverse
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator # 装饰器
+from django.views import View
+from django.conf import settings
+from root import models
+
+# Create your views here.
+
+class RootView(View):
+
+    def dispatch(self, request, *args, **kwargs):
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+
+        if request.path == '/':
+            return redirect('login')
+
+        elif request.path == '/login/':
+            ret = render(request, 'login.html')
+            ret.set_cookie('is_login', 'test_cookie')
+            return ret
+
+        elif request.path == '/home/':
+
+            return render(request, 'home.html')
+
+    def post(self, request, *args, **kwargs):
+
+        if request.path == '/login/':
+            data = request.POST.dict()
+
+            yn_exist_uname = models.UserInfo.objects.filter(username=data['username']).exists()
+            if yn_exist_uname:
+                pwd = models.UserInfo.objects.get(username=data['username']).password
+                if pwd == data['password']:
+                    ret = {'RETCODE': 200, "RETMSG": "SUCCESS"}
+                else:
+                    ret = {'RETCODE': 901, "RETMSG": "用户名或密码错误"}
+            else:
+                ret = {'RETCODE': 901, "RETMSG": "用户名或密码错误"}
+
+            return JsonResponse(ret)
+
+        elif request.path == '/home/':
+            files = request.FILES.getlist('file')
+            for file in files:
+                abs_path = os.path.join(settings.BASE_DIR, 'statics', 'data', file.name)
+                with open(abs_path, 'wb') as f:
+                    for chunk in file.chunks():
+                        f.write(chunk)
+            ret = {'RETCODE': 200, "RETMSG": "SUCCESS"}
+
+            return JsonResponse(ret)
