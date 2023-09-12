c6d44caf8ed0b62d41095e623928a4d07a83b16b
erwei.zheng <zhengew@foxmail.com>
2023-09-12 22:27:03 +0800

    day67_django_form组件、字段验证

--- day67_django中间件、form组件/exam/formtest/app01/views.py
+++ day67_django中间件、form组件/exam/formtest/app01/views.py
@@ -1,69 +1,93 @@
+import re
+
 from django.shortcuts import render, HttpResponse, redirect
 from django.http import JsonResponse
 from django import forms # form 组件
+from django.core.validators import RegexValidator # 字眼校验验证器
+from django.core.exceptions import ValidationError # 校验错误信息
 
 from app01 import models
-
 # Create your views here.
+
+# 自定义校验函数
+def mobile_validate(value):
+    mobile_re = re.compile(r'^(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$')
+    if not mobile_re.match(value):
+        raise ValidationError('手机号码格式错误')  #自定义验证规则的时候，如果不符合你的规则，需要自己发起错误
 
 class LoginForm(forms.Form):
 
     # 文本框
     name = forms.CharField(
+        required=True, # 默认必输
+        min_length=6,
         label='用户名',
         initial='大臂哥',
-        widget=forms.widgets.TextInput(attrs={'class': 'form-control'}),
+        help_text='这是输入用户名的地方，不能为空，最小6位字符',
+        # validators=[RegexValidator(r'^金瓶梅', '没看过不能通过'), RegexValidator(r'红旭妹妹$', '没看过红旭妹妹不能通过'),],
+
+        validators=[mobile_validate], # 自定义校验函数
+
+        error_messages={
+            'required': '不能为空',
+            'min_length': '不能太短',
+        },
+        # widget=forms.widgets.TextInput(attrs={'class': 'form-control'}),
+        widget=forms.widgets.TextInput,
     )
     # 密码框
     password = forms.CharField(
+        min_length=8,
+        max_length=10,
         label='密码',
-        widget=forms.widgets.PasswordInput(attrs={'class': 'form-control'}),
+        # widget=forms.widgets.PasswordInput(attrs={'class': 'form-control'}),
+        widget=forms.widgets.PasswordInput,
     )
 
-    # 单选按钮
-    sex = forms.ChoiceField(
-        label='性别',
-        choices=((1, '男'), (2, '女'), (3, '保密'),),
-        initial=3,
-        widget=forms.widgets.RadioSelect(),
-    )
-
-    # 下拉选择框
-    city = forms.ChoiceField(
-        label='城市',
-        choices=((1, '北京'), (2, '上海'), (3, '广州'),),
-        initial=1,
-        widget=forms.widgets.Select(),
-    )
-
-    # 多选复选框
-    hoby = forms.MultipleChoiceField(
-        label='爱好',
-        choices=((1, '抽烟'), (2, '喝酒'), (3, '烫头'),),
-
-        widget=forms.widgets.CheckboxSelectMultiple(),
-    )
-
-    # 多选下拉选择框
-    girls = forms.MultipleChoiceField(
-        label='女朋友',
-        choices=((1, '红旭妹妹'), (2, '雪飞姐姐'), (3, '详细哥哥'),),
-        widget=forms.widgets.SelectMultiple(),
-    )
-
-    # 单选 checkbox
-    status = forms.ChoiceField(
-        label='remeber me',
-        initial=True, # 默认勾选
-        choices=(('True', '红旭妹妹'), ('False', '雪飞姐姐')),
-        widget=forms.widgets.CheckboxInput,
-    )
-
-    # 日期
-    birthday = forms.CharField(
-        label='生日',
-        widget=forms.widgets.TextInput(attrs={'type': 'Date'}),
-    )
+    # # 单选按钮
+    # sex = forms.ChoiceField(
+    #     label='性别',
+    #     choices=((1, '男'), (2, '女'), (3, '保密'),),
+    #     initial=3,
+    #     widget=forms.widgets.RadioSelect(),
+    # )
+    #
+    # # 下拉选择框
+    # city = forms.ChoiceField(
+    #     label='城市',
+    #     choices=((1, '北京'), (2, '上海'), (3, '广州'),),
+    #     initial=1,
+    #     widget=forms.widgets.Select(),
+    # )
+    #
+    # # 多选复选框
+    # hoby = forms.MultipleChoiceField(
+    #     label='爱好',
+    #     choices=((1, '抽烟'), (2, '喝酒'), (3, '烫头'),),
+    #
+    #     widget=forms.widgets.CheckboxSelectMultiple(),
+    # )
+    #
+    # # 多选下拉选择框
+    # girls = forms.MultipleChoiceField(
+    #     label='女朋友',
+    #     choices=((1, '红旭妹妹'), (2, '雪飞姐姐'), (3, '详细哥哥'),),
+    #     widget=forms.widgets.SelectMultiple(),
+    # )
+    #
+    # # 单选 checkbox
+    # status = forms.ChoiceField(
+    #     label='remeber me',
+    #     initial=True, # 默认勾选
+    #     choices=(('True', '红旭妹妹'), ('False', '雪飞姐姐')),
+    #     widget=forms.widgets.CheckboxInput,
+    # )
+    #
+    # # 日期
+    # birthday = forms.CharField(
+    #     label='生日',
+    #     widget=forms.widgets.TextInput(attrs={'type': 'Date'}),
+    # )
 
 
 def index(request, *args, **kwargs):
@@ -76,17 +100,29 @@ def register(request, *args, **kwargs):
     form_obj = LoginForm()
 
     if request.method == 'GET':
-        return render(request, 'register.html', {'form_Obj': form_obj,})
+        return render(request, 'register.html', {'form_obj': form_obj,})
 
     elif request.method == 'POST':
 
-        uname = request.POST.get('username')
-        pwd = request.POST.get('password')
+        # uname = request.POST.get('username')
+        # pwd = request.POST.get('password')
+        #
+        # print(uname, pwd)
+        # if uname == 'zew' and pwd == '123456':
+        #     ret = {"RETCODE": 200, "RETMSG": "success"}
+        #     return JsonResponse(ret)
+        # else:
+        #     ret = {"RETCODE": "USERNAME_OR_PASSWORD_ERROR", "RETMSG": "用户名或密码错误"}
+        #     return JsonResponse(ret)
 
-        print(uname, pwd)
-        if uname == 'zew' and pwd == '123456':
-            ret = {"RETCODE": 200, "RETMSG": "success"}
-            return JsonResponse(ret)
+        # 演示 form 验证
+        form_obj = LoginForm(request.POST)
+        if form_obj.is_valid():  # 校验数据是否合法
+            print('true')
+            print(form_obj.cleaned_data)  # 通过校验的数据
+            return HttpResponse('200 ok')
         else:
-            ret = {"RETCODE": "USERNAME_OR_PASSWORD_ERROR", "RETMSG": "用户名或密码错误"}
-            return JsonResponse(ret)
+            print('false')
+            print(form_obj.errors)
+            return render(request, 'register.html', {'form_obj': form_obj})
+
